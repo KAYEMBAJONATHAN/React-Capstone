11cc1d9a9bc201a63a47b3953ef04afd9b14982c
kayemba <kayembejonathan5@gmail.com>
2023-05-25 15:23:13 +0200

    Add the setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Details from './Component/Details';
+import Home from './Component/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/details" element={<Details />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Details = () => (
+
+  <div>
+    Details
+    <h1>hhbhufbghgjph</h1>
+  </div>
+);
+
+export default Details;
